2c7023cf426aebeac4d148c428902f074bac7016
Satyaprakash Samantaray <satya_inet@yahoo.com>
2021-04-21 22:29:11 +0530

    Create MyFile1.cs

---
+++ MyFile1.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class MyDetails
+    {
+        public string FirstName { get; set; }        
+        public string DOB { get; set; }       
+    }
+}
