8211c766c899e228cbf307234c4147d90b7a127a
Satyaprakash Samantaray <satya_inet@yahoo.com>
2021-05-05 21:32:58 +0530

    Father name added

--- DateDetails.cs
+++ DateDetails.cs
@@ -9,21 +9,6 @@ namespace WebApplication1.Models
 {
     public class DateDetails
     {
-        public string ApplicantName { get; set; }        
-        public string DOB { get; set; }       
-        public string Age { get; set; }
-        public string DOJ { get; set; }
-        public string DOL { get; set; }
-        public string Expr { get; set; }
-        public string FatherName { get; set; }
-        public string MotherName { get; set; }
-        public string CountryName { get; set; }
-        public string StateName { get; set; }
-        public string Mobile { get; set; }      
-        public string Email { get; set; }
-
-        public string YearRange { get; set; }
-
-        public List<DateDetails> usersinfo { get; set; }
+       public string FatherName { get; set; } //added under main branch
     }
 }
